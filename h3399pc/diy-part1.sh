d7ad87347fcf26c370e1a9919713f1994056cb09
RockerHX <rockerhx@gmail.com>
2026-02-02 14:21:31 +0800

    优化版本命名
    由于测试时候同一天可能修改编译多个版本，所以这里需要把版本更新到具体的时间而不是某一天

--- h3399pc/diy-part1.sh
+++ h3399pc/diy-part1.sh
@@ -5,11 +5,13 @@
 # Author: RockerHX
 #===============================================
 
-# 修改版本为编译日期,数字类型
-date_version=$(date +"%Y%m")
+# 修改版本为编译日期+时间,便于一天多次编译时区分版本
+# 格式: YYYYMM-HHMM (例如: 202602-1425)
+date_version=$(date +"%Y%m-%H%M")
 echo $date_version > version
 
-# 为iStoreOS固件版本加上编译作者
+# 为iStoreOS固件版本加上编译作者和时间戳
+# 显示格式: 202602-1425 by RockerHX
 author="RockerHX"
 sed -i "s/DISTRIB_DESCRIPTION.*/DISTRIB_DESCRIPTION='%D %V ${date_version} by ${author}'/g" package/base-files/files/etc/openwrt_release
 sed -i "s/OPENWRT_RELEASE.*/OPENWRT_RELEASE=\"%D %V ${date_version} by ${author}\"/g" package/base-files/files/usr/lib/os-release
